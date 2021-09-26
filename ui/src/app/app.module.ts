2a73203e83663660eae6f24454cb4c8918641e4d
Sriram Ponangi <sr810178@dal.ca>
2021-09-26 16:11:56 -0300

    Adding main nav bar component

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddExpenseFormComponent } from './add-expense-form/add-expense-form.component';
 import { ExpensesPieChartComponent } from './expenses-pie-chart/expenses-pie-chart.component';
+import { MainNavBarComponent } from './main-nav-bar/main-nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddExpenseFormComponent,
-    ExpensesPieChartComponent
+    ExpensesPieChartComponent,
+    MainNavBarComponent
   ],
   imports: [
     BrowserModule,
