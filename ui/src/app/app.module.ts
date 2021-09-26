6c9db3f3e962279511e60f568fc81d067797e195
Sriram Ponangi <sr810178@dal.ca>
2021-09-26 15:44:46 -0300

    initial html with mock pie chart image

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddExpenseFormComponent } from './add-expense-form/add-expense-form.component';
+import { ExpensesPieChartComponent } from './expenses-pie-chart/expenses-pie-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddExpenseFormComponent
+    AddExpenseFormComponent,
+    ExpensesPieChartComponent
   ],
   imports: [
     BrowserModule,
