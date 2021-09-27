46857bb42d0b66073c77e39d4d14990b0d763053
Sriram Ponangi <sr810178@dal.ca>
2021-09-27 20:20:18 -0300

    creating components for sign-in and sign-up

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { MainNavBarComponent } from './main-nav-bar/main-nav-bar.component';
 import { HomePageComponent } from './main-nav-bar/home-page/home-page.component';
 import { AllExpensesPageComponent } from './main-nav-bar/all-expenses-page/all-expenses-page.component';
 import { ChartsModule } from 'ng2-charts';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
 
 @NgModule({
@@ -19,7 +21,9 @@ import { ChartsModule } from 'ng2-charts';
     ExpensesPieChartComponent,
     MainNavBarComponent,
     HomePageComponent,
-    AllExpensesPageComponent
+    AllExpensesPageComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
