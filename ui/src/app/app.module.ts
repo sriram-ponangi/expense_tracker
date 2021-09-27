7eca6da120ff36e21de9f75bf4bf95e7f34acb73
Sriram Ponangi <sr810178@dal.ca>
2021-09-27 20:18:12 -0300

    refactoring the component packages

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddExpenseFormComponent } from './add-expense-form/add-expense-form.component';
-import { ExpensesPieChartComponent } from './expenses-pie-chart/expenses-pie-chart.component';
+import { AddExpenseFormComponent } from './main-nav-bar/home-page/add-expense-form/add-expense-form.component';
+import { ExpensesPieChartComponent } from './main-nav-bar/home-page/expenses-pie-chart/expenses-pie-chart.component';
 import { MainNavBarComponent } from './main-nav-bar/main-nav-bar.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { AllExpensesPageComponent } from './all-expenses-page/all-expenses-page.component';
+import { HomePageComponent } from './main-nav-bar/home-page/home-page.component';
+import { AllExpensesPageComponent } from './main-nav-bar/all-expenses-page/all-expenses-page.component';
 import { ChartsModule } from 'ng2-charts';
 
 
