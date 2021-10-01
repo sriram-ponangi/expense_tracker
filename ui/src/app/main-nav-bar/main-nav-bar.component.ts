108935ee0ea182341b298611cf76ff76399840a1
Sriram Ponangi <19464979+sriram-ponangi@users.noreply.github.com>
2021-09-30 21:51:32 -0300

    Adding logout logic (#11)

--- ui/src/app/main-nav-bar/main-nav-bar.component.ts
+++ ui/src/app/main-nav-bar/main-nav-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CognitoUserPool } from 'amazon-cognito-identity-js';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-main-nav-bar',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainNavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onLogout(): void {
+    let poolData = {
+      UserPoolId: environment.cognitoUserPoolId,
+      ClientId: environment.cognitoAppClientId
+    };
+    let userPool = new CognitoUserPool(poolData);
+    let cognitoUser = userPool.getCurrentUser();
+    console.log(cognitoUser?.getUsername());
+
+    cognitoUser?.getSession((err: any, session: any) => {
+      if (err) {
+        console.log(err);
+      } else if (!session.isValid()) {
+        console.log("Invalid session.");
+      } else {
+        console.log("IdToken: " + session.getIdToken().getJwtToken());
+      }
+    });
+    
+    
+    cognitoUser?.signOut();
+
+    cognitoUser?.getSession((err: any, session: any) => {
+      if (err) {
+        console.log(err);
+      } else if (!session.isValid()) {
+        console.log("Invalid session.");
+      } else {
+        console.log("IdToken: " + session.getIdToken().getJwtToken());
+      }
+    });
+    
+    this.router.navigate(["sign-in"])
   }
 
 }
