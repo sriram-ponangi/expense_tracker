8bebbeaf696726f5e49526230963cfdabeed5a69
Sriram Ponangi <19464979+sriram-ponangi@users.noreply.github.com>
2021-09-30 21:29:47 -0300

    Adding sign - in logic with cognito (#9)

--- ui/src/app/auth/sign-in/sign-in.component.ts
+++ ui/src/app/auth/sign-in/sign-in.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationDetails, CognitoUser, CognitoUserPool } from 'amazon-cognito-identity-js';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,12 +10,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
-  email:string = '';
-  password:string = '';
-  
-  constructor() { }
+  isLoading: boolean = false;
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  onSignIn(form: NgForm) {
+    if (form.valid) {
+      this.isLoading = true;
+      let authenticationDetails = new AuthenticationDetails({
+        Username: this.email,
+        Password: this.password,
+      });
+      let poolData = {
+        UserPoolId: environment.cognitoUserPoolId, // Your user pool id here
+        ClientId: environment.cognitoAppClientId // Your client id here
+      };
+
+      let userPool = new CognitoUserPool(poolData);
+      let userData = { Username: this.email, Pool: userPool };
+      var cognitoUser = new CognitoUser(userData);
+      cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: (result) => {
+
+          // console.log('access token + ' + result.getAccessToken().getJwtToken());
+          // console.log('id token + ' + result.getIdToken().getJwtToken());
+          // console.log('refresh token + ' + result.getRefreshToken().getToken());
+          console.log(result.isValid());
+          
+          this.router.navigate(["home"])
+        },
+        onFailure: (err) => {
+          alert(err.message || JSON.stringify(err));
+          this.isLoading = false;
+        },
+      });
+    }
+  }
+
 
 }
