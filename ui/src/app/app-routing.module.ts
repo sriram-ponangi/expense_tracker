4ade25d7665b6610ebefb4ecf4c52f08664f2d4b
Sriram Ponangi <sr810178@dal.ca>
2021-10-02 12:32:06 -0300

    Adding route gaurds for authentication

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardGuard } from './auth/auth-guard.guard';
+import { AuthService } from './auth/auth.service';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { AllExpensesPageComponent } from './main-nav-bar/all-expenses-page/all-expenses-page.component';
 import { HomePageComponent } from './main-nav-bar/home-page/home-page.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomePageComponent },
-  { path: 'all-expenses', component: AllExpensesPageComponent },
+  { path: 'home', component: HomePageComponent, canActivate : [AuthGuardGuard] },
+  { path: 'all-expenses', component: AllExpensesPageComponent, canActivate : [AuthGuardGuard] },
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent }
   
@@ -15,6 +17,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthService]
 })
 export class AppRoutingModule { }
